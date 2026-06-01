9766d5c73ecf504b3f2288413b12950e329d5e9f
Ritika Kunwar <kunwarritika46@gmail.com>
2026-06-01 14:18:59 +0545

    update

---
+++ src/types/custom.d.ts
@@ -0,0 +1,14 @@
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.less';
+declare module '*.styl';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.webp';
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
